42400f6f8ef506f7e9efc3c0e65d35a4755c373b
Tiresa Dombrowski <35544695+tjdombrowski@users.noreply.github.com>
2019-05-01 16:16:21 -0500

    Recoded how all the data is received from the api using expand

--- src/app/swapi.service.ts
+++ src/app/swapi.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { merge } from 'rxjs';
+import { merge, empty } from 'rxjs';
+import { expand } from 'rxjs/operators';
+import { AnyMxRecord } from 'dns';
 
 @Injectable({
   providedIn: 'root'
@@ -10,9 +12,17 @@ export class SwapiService {
   constructor(private httpSvc: HttpClient) { }
 
   getPlanets() {
-    const p1 =  this.httpSvc.get("https://swapi.co/api/planets");
-    const p2 = this.httpSvc.get("https://swapi.co/api/planets?page=2");
+    // const p1 =  this.httpSvc.get("https://swapi.co/api/planets");
+    // const p2 = this.httpSvc.get("https://swapi.co/api/planets?page=2");
+    // return merge(p1, p2);
 
-    return merge(p1, p2);
+    return this.httpSvc.get("https://swapi.co/api/planets").pipe(
+      expand(data => (<any>data).next ? 
+      this.httpSvc.get((<any>data).next)
+      : empty())
+      );
+
+    //expand is used to recursively make calls
+    //the value of next is null in the api if there is no further page
   }
 }
