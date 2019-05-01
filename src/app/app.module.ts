171116b250d355b668a42e8a163e7a62999ce35c
Tiresa Dombrowski <35544695+tjdombrowski@users.noreply.github.com>
2019-05-01 15:05:10 -0500

    Added dependencies

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SharedDataService } from './shared-data.service';
+import { SwapiService } from './swapi.service';
 
 @NgModule({
   declarations: [AppComponent],
@@ -20,7 +22,9 @@ import { AppRoutingModule } from './app-routing.module';
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    SharedDataService,
+    SwapiService
   ],
   bootstrap: [AppComponent]
 })
